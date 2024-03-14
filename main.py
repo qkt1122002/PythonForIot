d5b2d2396aa108c51308a12273a881563569e4f7
qkt1122002 <qkt1122002@gmail.com>
2024-03-14 22:06:00 +0700

    ve do thi

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-import matplotlib
-print(matplotlib.__version__)
+import matplotlib.pyplot as plt
+# import numpy as np
+
+X = [0,3,5]
+Y = [3,-4,8]
+
+plt.subplot(2, 2, 1)
+plt.plot(X, Y, marker='o')
+plt.title('Do thi')
+plt.xlabel('Truc X')
+plt.ylabel('Truc Y')
+
+# Ve do thi duong thang y = 2x + 2
+# Trong doan [-10, 10]
+x = []
+y =[]
+i = -5
+while(i <= 5):
+    x.append(i)
+    i = i + 0.01
+for a in x:
+    s = 2*a*a + 3 * a - 1
+    y.append(s)
+
+plt.subplot(2, 2, 2)
+plt.plot(x, y)
+
+plt.title("do thi", loc='right')
+plt.xlabel("Truc x")
+plt.ylabel("Truc y")
+plt.grid()
+plt.show()
---
+++ pie.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+X = [25,25,35,5,20]
+plt.pie(X)
+plt.show()
